360a8608f4d4400bdbbaf79a371bec4c4f85d0d8
Jinx-FX <2674802603@qq.com>
2021-12-21 21:57:56 +0800

    fix

--- main_rpi.py
+++ main_rpi.py
@@ -38,7 +38,7 @@ def run_ogl(fidr, fobj_pkl, fobj):
     param.sel_pos = False
 
     pygame.init()
-    viewport = (120, 120)
+    viewport = (200, 200)
     param.viewport = viewport
     screen = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
     param.screen = screen
